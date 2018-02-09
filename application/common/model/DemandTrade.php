a6b4528f81383733a7ad6a37e0edb12896d348b2
licai <php_programmer@163.com>
2018-02-09 11:55:54 +0800

    ffff

---
+++ application/common/model/DemandTrade.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\common\model;
+
+use think\Model;
+
+class DemandTrade extends Model
+{
+    // 指定表名,不含前缀
+    protected $name = 'demand_trade';
+    
+}
---
+++ application/index/controller/Demand.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @Author: Marte
+ * @Date:   2018-01-25 17:46:09
+ * @Last Modified by:   Marte
+ * @Last Modified time: 2018-02-09 11:52:52
+ */
+namespace app\index\controller;
+use app\admin\Controller;
+use think\Session;
+
+use app\common\model\Cases as C;
+use app\common\model\Type as T;
+use app\common\model\Specific as S;
+use app\common\model\System as SY;
+use app\common\model\CasesOrder as CO;
+use app\common\model\DemandType as DT;
+use app\common\model\DemandTrade as DTR;
+
+/**
+* 案例
+*/
+class Demand extends Yang
+{
+    public function publish()
+    {
+        $demandtrade = DTR::select();
+        $demandtype = DT::select();
+        $this->assign('demandtrade',$demandtrade);
+        $this->assign('demandtype',$demandtype);
+        return $this->fetch();
+    }
+
+}
