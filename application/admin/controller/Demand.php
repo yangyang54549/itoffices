c7fa98bdb3a7f8ba2866b0985afd7d1fb0f1e271
licai <php_programmer@163.com>
2018-04-16 16:36:30 +0800

    aa

--- application/admin/controller/Demand.php
+++ application/admin/controller/Demand.php
@@ -4,6 +4,10 @@ namespace app\admin\controller;
 \think\Loader::import('controller/Controller', \think\Config::get('traits_path') , EXT);
 
 use app\admin\Controller;
+use think\Db;
+use think\Loader;
+use think\exception\HttpException;
+use think\Config;
 
 class Demand extends Controller
 {
@@ -11,5 +15,32 @@ class Demand extends Controller
     // 方法黑名单
     protected static $blacklist = [];
 
-    
+    /**
+     * 首页
+     * @return mixed
+     */
+    public function index()
+    {
+        $model = $this->getModel();
+
+        // 列表过滤器，生成查询Map对象
+        $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete]);
+
+        // 特殊过滤器，后缀是方法名的
+        $actionFilter = 'filter' . $this->request->action();
+        if (method_exists($this, $actionFilter)) {
+            $this->$actionFilter($map);
+        }
+
+        // 自定义过滤器
+        if (method_exists($this, 'filter')) {
+            $this->filter($map);
+        }
+
+        $this->datalist($model, $map);
+        $trade = Db::table('tp_demand_trade')->select();//行业
+        $type = Db::table('tp_demand_type')->select();//类型
+
+        return $this->view->fetch();
+    }
 }
