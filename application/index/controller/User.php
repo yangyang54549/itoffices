7eb4dde9db73cc34f9e34024dba810ec8d3ee765
licai <php_programmer@163.com>
2018-04-18 16:07:40 +0800

    aaa

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -3,10 +3,11 @@
  * @Author: Marte
  * @Date:   2018-04-17 15:05:19
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-04-18 10:52:18
+ * @Last Modified time: 2018-04-18 15:50:05
  */
 namespace app\index\controller;
 use app\admin\Controller;
+use think\Session;
 use app\common\model\User as U;
 use app\common\model\UserEducation;
 use app\common\model\UserExperience;
@@ -17,13 +18,13 @@ class User  extends Yang
 {
     public function index()
     {
-        $user = U::where('id',1)->find();
+        $user = U::where('id',Session::get('user.id'))->find();
         $this->assign('user',$user);
         return $this->fetch();
     }
     public function resume()
     {
-        $user = U::where('id',1)->find();
+        $user = U::where('id',Session::get('user.id'))->find();
         $UserEducation = UserEducation::where(['user_id'=>$user['id']])->select();
         $UserExperience = UserExperience::where(['user_id'=>$user['id']])->select();
         $UserProduction = UserProduction::where(['user_id'=>$user['id']])->select();
@@ -45,10 +46,19 @@ class User  extends Yang
     }
     public function bianji1()
     {
+        $UserSkill = UserSkill::where(['user_id'=>Session::get('user.id')])->select();
+        $this->assign('skill',$UserSkill);
         return $this->fetch();
     }
     public function bianji2()
     {
+        $user = U::where('id',Session::get('user.id'))->find();
+        $UserEducation = UserEducation::where(['user_id'=>$user['id']])->select();
+        $UserExperience = UserExperience::where(['user_id'=>$user['id']])->select();
+        $this->assign('education',$UserEducation);
+        $this->assign('experience',$UserExperience);
+        $this->assign('user',$user);
+
         return $this->fetch();
     }
 }
