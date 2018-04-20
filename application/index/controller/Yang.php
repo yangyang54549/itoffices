ee442a5560673a67f73699d2dff527a7aa30bc6b
licai <php_programmer@163.com>
2018-04-20 16:11:24 +0800

    aaaa

--- application/index/controller/Yang.php
+++ application/index/controller/Yang.php
@@ -3,19 +3,35 @@
  * @Author: Marte
  * @Date:   2018-01-25 17:47:48
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-01-31 18:06:15
+ * @Last Modified time: 2018-04-20 12:04:56
  */
 
 namespace app\index\controller;
 use think\Controller;
 use think\Session;
 use think\Cookie;
+use app\common\model\User;
 
 class yang extends Controller
 {
+    protected $arr = ['Cases/index','Cases/inside','Cases/computers','Cases/phones','Cases/order','Demand/index','Demand/inside','Index/index','Index/index_bak','Login/codemsg','Login/login','Login/checkindex','Login/admin'];
+    protected $ret = ['code'=>1,'data'=>'','msg'=>'提交成功'];//1是成功 -200是失败
+
     public function __construct()
     {
-        $this->ret = ['code'=>1,'data'=>'','msg'=>'提交成功'];//1是成功 -200是失败
         parent::__construct();
+        $request=  \think\Request::instance();
+        $con = $request->controller();
+        $act = $request->action();
+        $url = $con.'/'.$act;
+        if (!in_array($url,$this->arr)) {
+
+            $user = Session::get('user');
+            if (!isset($user)) {
+                //未登录
+                $this->redirect('index/login/login');
+            }
+
+        }
     }
 }
