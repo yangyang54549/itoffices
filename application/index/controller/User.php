4750ad4a8ef1398248505ba01888deefd522fecd
licai <php_programmer@163.com>
2018-04-25 17:25:55 +0800

    aa

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -3,7 +3,7 @@
  * @Author: Marte
  * @Date:   2018-04-17 15:05:19
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-04-25 14:54:35
+ * @Last Modified time: 2018-04-25 17:19:20
  */
 namespace app\index\controller;
 use app\admin\Controller;
@@ -83,10 +83,24 @@ class User  extends Yang
         $sdemand = [];
         foreach ($apply as $k => $v) {
             $de = Demand::where(['id'=>$v['demand_id']])->find();//我发布的需求
+
+            if (isset($de['apply_id'])) {
+                if ($de['apply_id']==$v['id']) {
+                    $de['is_apply_id'] = 1;//申请的需求人选中了我
+                }elseif($de['apply_id']!=$v['id']){
+                    $de['is_apply_id'] = 2;//申请的需求人没有选中我
+                }
+
+                $app = Apply::where(['id'=>$de['apply_id']])->find();
+                $de['apply_id'] = $app;
+
+            }else{
+                    $de['is_apply_id'] = 0;//申请的需求人还没有选择开发者
+            }
+
             $sdemand[$k] = $de;
         }
-
-        $sdemand = Demand::where(['user_id'=>Session::get('user.id')])->select();//我申请的需求
+        //dump($sdemand);die;
         $demandtrade = DemandTrade::select();
         $demandtype = DemandType::select();
         $this->assign('demand',$demand);
@@ -109,14 +123,29 @@ class User  extends Yang
             }
         }
 
-        $apply = Apply::where(['user_id'=>Session::get('user.id')])->select();
-        $sdemand = [];
-        foreach ($apply as $k => $v) {
-            $de = Demand::where(['id'=>$v['demand_id']])->find();//我发布的需求
-            $sdemand[$k] = $de;
-        }
+        // $apply = Apply::where(['user_id'=>Session::get('user.id')])->select();
+        // $sdemand = [];
+        // foreach ($apply as $k => $v) {
+        //     $de = Demand::where(['id'=>$v['demand_id']])->find();//我发布的需求
 
-        // $sdemand = Demand::where(['user_id'=>Session::get('user.id')])->select();//我申请的需求
+        //     if (isset($de['apply_id'])) {
+        //         if ($de['apply_id']==$v['id']) {
+        //             $de['is_apply_id'] = 1;//申请的需求人选中了我
+        //         }elseif($de['apply_id']!=$v['id']){
+        //             $de['is_apply_id'] = 2;//申请的需求人没有选中我
+        //         }
+
+        //         $app = Apply::where(['id'=>$de['apply_id']])->find();
+        //         $de['apply_id'] = $app;
+
+        //     }else{
+        //             $de['is_apply_id'] = 0;//申请的需求人还没有选择开发者
+        //     }
+
+        //     $sdemand[$k] = $de;
+        // }
+
+        $sdemand = Demand::where(['user_id'=>Session::get('user.id')])->select();//我申请的需求
         $demandtrade = DemandTrade::select();
         $demandtype = DemandType::select();
         $this->assign('demand',$demand);
@@ -142,16 +171,16 @@ class User  extends Yang
         $user_id = input('user_id');
         $demand_id = input('demand_id');
         Demand::where(['id'=>$demand_id])->update(['schedule'=>3]);
-        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['xstatus' => 2,'sstatus'=>2]);
+        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['xstatus' => 2]);
         return json($this->ret);
     }
 
     //申请方确认干活
     public function order_gan()
     {
-        $user_id = input('user_id');
+        $user_id = Session::get('user.id');
         $demand_id = input('demand_id');
-        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['xstatus' => 2,'sstatus'=>2]);
+        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['sstatus'=>1]);
         return json($this->ret);
     }
 
@@ -161,7 +190,7 @@ class User  extends Yang
         $user_id = input('user_id');
         $demand_id = input('demand_id');
         Demand::where(['id'=>$demand_id])->update(['schedule'=>3]);
-        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['xstatus' => 2,'sstatus'=>2]);
+        Apply::where(['user_id' => $user_id,'demand_id'=>$demand_id])->update(['sstatus'=>2]);
         return json($this->ret);
     }
 
