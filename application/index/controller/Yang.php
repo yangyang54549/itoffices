a9de0ff434d0eee57103263fe61d73870ff12711
licai <php_programmer@163.com>
2018-05-09 10:04:00 +0800

    aaa

--- application/index/controller/Yang.php
+++ application/index/controller/Yang.php
@@ -3,7 +3,7 @@
  * @Author: Marte
  * @Date:   2018-01-25 17:47:48
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-05-04 17:15:08
+ * @Last Modified time: 2018-05-09 09:44:48
  */
 
 namespace app\index\controller;
@@ -14,7 +14,7 @@ use app\common\model\User;
 
 class yang extends Controller
 {
-    protected $arr = ['Cases/index','Cases/inside','Cases/computers','Cases/phones','Cases/order','Demand/index','Demand/inside','Index/index','Index/index_bak','Login/codemsg','Login/login','Login/checkindex','Login/admin','Index/one'];
+    protected $arr = ['Cases/index','Cases/inside','Cases/computers','Cases/phones','Cases/order','Demand/index','Demand/inside','Index/index','Index/index_bak','Login/codemsg','Login/login','Login/checkindex','Login/admin','Index/one','Index/lxwm'];
     protected $ret = ['code'=>1,'data'=>'','msg'=>'提交成功'];//1是成功 -200是失败
 
     public function __construct()
