e3d01957f234b9705fec479260a42210b6fc6877
赵 <34564562+Decembers@users.noreply.github.com>
2018-06-06 19:03:54 +0800

    123123123

--- public/static/index/js/login.js
+++ public/static/index/js/login.js
@@ -21,29 +21,4 @@ function loginNone(){
 				$(".login_background").css("display","none");
 				$(".login").css("display","none");
 			}
-			function login1s() {
-                if($("#phones").val() == "") {
-                    block("请输入手机号码");
-                }  else {
-                	if(!(/^1[3456789]\d{9}$/.test($("#phones").val()))) {
-						block("手机号码有误，请重填");
-					} else {
-						if($("#j_captcha").val()!=""){
-
-							var mobile = $("#phones").val();
-							var code = $("#j_captcha").val();
-							$.post("{:url('login/checkindex')}",{mobile:mobile,code:code},function(data) {
-								if (data.code==1) {
-									block("登录成功");
-									window.location.href='{:url("user/index")}';
-								}else{
-									block(data.msg);
-								}
-							})
-						}else{
-							block("请输入短信验证码");
-						}
-					}
-				}
-			}
-
+			
